4d94fcf9446f99d2fbe869af7b3f963d1fb810bf
hemateja566 <htre2004@gmail.com>
2026-03-18 12:50:43 +0530

    fee calculator along with datatypes

---
+++ day2.py
@@ -0,0 +1,4 @@
+Python 3.13.10 (tags/v3.13.10:4fd8843, Dec  2 2025, 15:08:18) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=== RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python313/day2.py ===
