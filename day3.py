ab88ffb205b48fa939c37f63e5c5ee7d4ee403e2
hemateja566 <htre2004@gmail.com>
2026-03-20 10:39:02 +0530

    assignment 3

---
+++ day3.py
@@ -0,0 +1,14 @@
+print("salary")
+print("====================")
+basic_salary=60000
+travel_expenses=800
+pf=2000
+allowances=5000
+total=basic_salary+travel_expenses+pf+allowances
+print("====================")
+print("basic salary",basic_salary)
+print("travel expenses",travel_expenses)
+print("provident funds",pf)
+print("allowances",allowances)
+print("=====================")
+print("total salary:",total)
